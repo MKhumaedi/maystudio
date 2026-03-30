a4f61e317fe70a7d6e3e4fabc1aef1886a14d808
edi <108387177+MKhumaedi@users.noreply.github.com>
2026-03-30 10:18:30 +0700

    Replace logo image with text in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,7 +74,7 @@ const Header = () => {
             <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
                 <a className="block w-[12rem] xl:mr-8" href="#hero">
                     
-                    <img src={maystudio} width={150} alt="Maystudio" />
+                    <h3>Maystudio</h3>
                 </a>
 
                 <nav className={`${openNavigation ? "flex" : "hidden"} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
